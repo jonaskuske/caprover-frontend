9b00202fac7d925063efa4129ab95ec3226bad17
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2022-11-07 16:23:09 +0100

    refator: replace dep moment with Intl APIs

--- src/containers/global/Timestamp.tsx
+++ src/containers/global/Timestamp.tsx
@@ -1,14 +1,14 @@
 import { Tooltip } from 'antd'
-import moment from 'moment'
-import React, { Component } from 'react'
+import { Component } from 'react'
+import { formatToRelativeTime } from '../../utils/Utils'
 
 export default class Timestamp extends Component<{ timestamp: string }, {}> {
-    render() {
-        const timestamp = this.props.timestamp
-        return (
-            <Tooltip title={moment(new Date(timestamp)).fromNow()}>
-                <span>{new Date(timestamp).toLocaleString()}</span>
-            </Tooltip>
-        )
-    }
+	render() {
+		const timestamp = this.props.timestamp
+		return (
+			<Tooltip title={formatToRelativeTime(new Date(timestamp))}>
+				<span>{new Date(timestamp).toLocaleString()}</span>
+			</Tooltip>
+		)
+	}
 }
